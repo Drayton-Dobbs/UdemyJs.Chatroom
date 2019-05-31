feef972cfdc9cd6c8fe99e17ddb96bb262bcc596
James Dobbs <jamesdraytondobbs@gmail.com>
2019-05-31 14:26:35 -0500

    completed chatroom

--- scripts/chat.js
+++ scripts/chat.js
@@ -10,6 +10,7 @@ class Chatroom {
             this.room = room;
             this.username = username;
             this.chats = db.collection('chats');
+            this.unsub;
         }
         async addChat(message){
             // format a chat object
@@ -26,24 +27,30 @@ class Chatroom {
         }
         // real=time listener
         getChats(callback){
-            this.chats
-                .onSnapshot(snapshot => {
+            this.unsub = this.chats
+            .where('room', '==', this.room)
+            .orderBy('created_at')
+            .onSnapshot(snapshot => {
                     snapshot.docChanges().forEach(change => {
                         if(change.type === 'added'){
                             // update UI
-                            callback(change.doc.data())
+                            callback(change.doc.data());
                         }
                     });
                 });
         }
+        updateName(username){
+            this.username = username;
+            localStorage.setItem('username', username);
+        }
+        updateRoom(room){
+            this.room = room;
+            console.log('room updated');
+            if(this.unsub) {
+                this.unsub();
+            }
+            
+        }
+};
 
-}
 
-const chatroom = new Chatroom();
-
-
-
-
-chatroom.getChats((data) => {
-    console.log(data);
-});
